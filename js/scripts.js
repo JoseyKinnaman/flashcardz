c11374536891e7b23ea13fa5fac68930fb409d6f
Jozy Kinnaman <jozypants@gmail.com>
2020-03-24 14:11:05 -0700

    add html structure, css styling, and js functionality

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("#javascript").click(function() {
+    $("#javaAnswer").toggle();
+  });
+  $("#operators").click(function() {
+    $("#operatorAnswer").toggle();
+  });
+  $("#variables").click(function() {
+    $("#variableAnswer").toggle();
+  });
+  $("#functions").click(function() {
+    $("#functionAnswer").toggle();
+  });
+  $("#methods").click(function() {
+    $("#methodAnswer").toggle();
+  });
+});
