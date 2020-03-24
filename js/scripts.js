e8658c48c92cb31454e2d92041777bea0ba755b4
Jozy Kinnaman <jozypants@gmail.com>
2020-03-24 14:36:53 -0700

    add toggle speeds to js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,51 @@
 $(document).ready(function() {
   $("#javascript").click(function() {
-    $("#javaAnswer").toggle();
+    $("#javaAnswer").fadeToggle(1000);
+    $("#javascript").fadeToggle(1000);
   });
+
+  $("#javaAnswer").click(function() {
+    $("#javaAnswer").fadeToggle(1000);
+    $("#javascript").fadeToggle(1000);
+  });
+
   $("#operators").click(function() {
-    $("#operatorAnswer").toggle();
+    $("#operatorAnswer").fadeToggle(200);
+    $("#operators").fadeToggle(200);
   });
+
+  $("#operatorAnswer").click(function() {
+    $("#operators").fadeToggle(200);
+    $("#operatorAnswer").fadeToggle(200);
+  });
+
   $("#variables").click(function() {
-    $("#variableAnswer").toggle();
+    $("#variableAnswer").fadeToggle(200);
+    $("#variables").fadeToggle(200);
   });
+
+  $("#variablesAnswer").click(function() {
+    $("#variables").fadeToggle(200);
+    $("#variableAnswer").fadeToggle(200);
+  });
+
   $("#functions").click(function() {
-    $("#functionAnswer").toggle();
+    $("#functionAnswer").fadeToggle(200);
+    $("#functions").fadeToggle(200);
   });
+
+  $("#functionsAnswer").click(function() {
+    $("#functions").fadeToggle(200);
+    $("#functionsAnswer").fadeToggle(200);
+  });
+
   $("#methods").click(function() {
-    $("#methodAnswer").toggle();
+    $("#methodAnswer").fadeToggle(200);
+    $("#methods").fadeToggle(200);
+  });
+
+  $("#methodsAnswer").click(function() {
+    $("#methods").fadeToggle(200);
+    $("#methodsAnswer").fadeToggle(200);
   });
 });
